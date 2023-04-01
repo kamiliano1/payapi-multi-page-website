073903bcaf4bb1037facf1baed4f90168595396f
kamiliano1 <kamiliano1991@gmail.com>
2023-04-01 12:15:36 +0200

    App

--- src/Layout/Navbar/HorizontalNav.tsx
+++ src/Layout/Navbar/HorizontalNav.tsx
@@ -6,19 +6,19 @@ const HorizontalNav: React.FC = () => {
   return (
     <div className="hidden text-center items-center text-sanJuanBlue sm:flex  w-full ">
       <Link
-        className="text-100 font-bold opacity-70 mr-10 hover:opacity-100"
+        className="text-100 font-bold opacity-80 mr-10 hover:opacity-100"
         href="pricing"
       >
         Pricing{" "}
       </Link>
       <Link
-        className="text-100 font-bold opacity-70 mr-10 hover:opacity-100"
+        className="text-100 font-bold opacity-80 mr-10 hover:opacity-100"
         href="about"
       >
         About{" "}
       </Link>
       <Link
-        className="text-100 font-bold opacity-70 mr-10 hover:opacity-100"
+        className="text-100 font-bold opacity-80 mr-10 hover:opacity-100"
         href="contact"
       >
         Contact{" "}
